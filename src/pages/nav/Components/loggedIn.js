ff1a3a0cab32c6f7790d1a682fa90bf4993ffa25
Luke <user@pop-os.localdomain>
2023-04-17 13:43:14 -0500

    update nav styling based on location

--- src/pages/nav/Components/loggedIn.js
+++ src/pages/nav/Components/loggedIn.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import homeFeedWhite from "../../../assets/images/home-white.png";
 import homeFeedGreen from "../../../assets/images/home-green.png";
 import notificationsWhite from "../../../assets/images/notifications-white.png";
@@ -9,22 +9,49 @@ import userWhite from "../../../assets/images/user-white.png";
 import userGreen from "../../../assets/images/user-white.png";
 import postWhite from "../../../assets/images/post-white.png";
 import postGreen from "../../../assets/images/post-green.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { createPortal } from "react-dom";
 import Post from "../../post/Post";
 
 const LoggedIn = () => {
   const [showPost, setShowPost] = useState(false);
+  const location = useLocation();
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    setUrl(location.pathname);
+  }, [location]);
+
   return (
     <div className="directory-container">
       <div className="directory-profile-container">
-        <Link to={"/profile"}>
-          <img src={userWhite} className="user-logo" alt="user logo"></img>
+        <Link
+          to={"/profile"}
+          className={"nav-underline" + (url === "/profile" ? " active" : "")}
+        >
+          <img
+            src={userWhite}
+            className="user-logo"
+            alt="user logo"
+            onMouseOver={(e) => (e.currentTarget.style.borderColor = "#86c323")}
+            onMouseLeave={(e) =>
+              (e.currentTarget.style.borderColor = "#222629")
+            }
+          ></img>
         </Link>
       </div>
       <div className="directory-feed-container">
-        <Link to={"/"}>
-          <img src={homeFeedWhite} className="feed-logo" alt="feed logo"></img>
+        <Link
+          to={"/"}
+          className={"nav-underline" + (url === "/" ? " active" : "")}
+        >
+          <img
+            src={homeFeedWhite}
+            className="feed-logo"
+            alt="feed logo"
+            onMouseOver={(e) => (e.currentTarget.src = homeFeedGreen)}
+            onMouseLeave={(e) => (e.currentTarget.src = homeFeedWhite)}
+          ></img>
         </Link>
       </div>
       <div className="directory-post-container">
@@ -33,6 +60,8 @@ const LoggedIn = () => {
           className="post-logo"
           alt="post logo"
           onClick={() => setShowPost(true)}
+          onMouseOver={(e) => (e.currentTarget.src = postGreen)}
+          onMouseLeave={(e) => (e.currentTarget.src = postWhite)}
         ></img>
         {showPost &&
           createPortal(
@@ -41,21 +70,33 @@ const LoggedIn = () => {
           )}
       </div>
       <div className="directory-notifications-container">
-        <Link to={"/notifications"}>
+        <Link
+          to={"/notifications"}
+          className={
+            "nav-underline" + (url === "/notifications" ? " active" : "")
+          }
+        >
           <img
             src={notificationsWhite}
             className="notifications-logo"
             alt="notification logo"
+            onMouseOver={(e) => (e.currentTarget.src = notificationsGreen)}
+            onMouseLeave={(e) => (e.currentTarget.src = notificationsWhite)}
           ></img>
         </Link>
         <div className="notifcations-sticker"></div>
       </div>
       <div className="directory-messages-container">
-        <Link to={"/messages"}>
+        <Link
+          to={"/messages"}
+          className={"nav-underline" + (url === "/messages" ? " active" : "")}
+        >
           <img
             src={messagesWhite}
             className="messages-logo"
             alt="messages logo"
+            onMouseOver={(e) => (e.currentTarget.src = messagesGreen)}
+            onMouseLeave={(e) => (e.currentTarget.src = messagesWhite)}
           ></img>
         </Link>
         <div className="messages-sticker"></div>
